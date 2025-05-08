096c48a4d0ecd166c6d3dde516fd2674971e1edd
Louis Fernando <ferlouis55@gmail.com>
2025-05-08 08:25:45 +0700

    Initial Commit

---
+++ ADA_Challenge3/ADA_Challenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  ADA_Challenge3App.swift
+//  ADA_Challenge3
+//
+//  Created by Louis Fernando on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ADA_Challenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADA_Challenge3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ADA_Challenge3
+//
+//  Created by Louis Fernando on 08/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
